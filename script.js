a830e96a0522853d8c51c9e676d2b77e85c7b49c
Aditya Hansraj <130595579+trafalgarLaw-3b@users.noreply.github.com>
2023-12-16 20:48:49 +0530

    New minor Project "To-Do List"

---
+++ script.js
@@ -0,0 +1,46 @@
+$(document).ready(() => {
+    const input = $("#input");
+    const list = $("#list");
+
+    function saveData() {
+        localStorage.setItem("data", list.html());
+    }
+
+    function getData() {
+        list.html(localStorage.getItem("data"));
+    }
+
+    $("#add").submit(function(event) {
+        event.preventDefault();
+
+        var newTask = input.val();
+        var li = $("<li class='unchecked'>");
+        var cross = $("<span>")
+        cross.html("\u00d7");
+        li.text(newTask);
+        li.append(cross);
+        li.hide().appendTo(list).slideDown(function(){
+            saveData();
+        });
+        input.val('');
+        
+    });
+
+    $("span").click(function(){
+        $(this).parent().remove();
+    })
+
+    list.on("click", "li", function() {
+        $(this).toggleClass('checked unchecked');
+        saveData();
+    });
+
+    list.on("click", "span", function() {
+        $(this).parent().slideUp(400, function() {
+            $(this).remove();
+            saveData();
+        });
+    });
+
+    getData();
+});
