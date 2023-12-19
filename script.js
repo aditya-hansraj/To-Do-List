53fd7c7c978eedb1592a79f67ed31005b217b7d5
Aditya Hansraj <130595579+trafalgarLaw-3b@users.noreply.github.com>
2023-12-19 17:26:16 +0530

    Added Options

--- script.js
+++ script.js
@@ -22,25 +22,59 @@ $(document).ready(() => {
         li.hide().appendTo(list).slideDown(function(){
             saveData();
         });
+        check();
         input.val('');
         
     });
 
-    $("span").click(function(){
-        $(this).parent().remove();
-    })
-
     list.on("click", "li", function() {
         $(this).toggleClass('checked unchecked');
+        check();
+        $("#opt ul").fadeOut('fast');
         saveData();
     });
 
     list.on("click", "span", function() {
         $(this).parent().slideUp(400, function() {
             $(this).remove();
+            check();
+            $("#opt ul").fadeOut('fast');
             saveData();
         });
     });
 
+    $("#opt i").click(function(event) {
+        event.stopPropagation();
+        $("#opt ul").fadeIn('fast');
+    })
+
+    $(document).on('click', ':not(opt)', function(event){
+        if (!$(event.target).closest('ul').length) {
+            $("#opt ul").fadeOut('fast');
+        }
+    })
+
+    $("#check").click(function() {
+        if($('#list li').length > $('.checked').length){
+            $('#list li').attr('class', 'checked'); 
+            $('#check').text("Uncheck All");
+        }else{
+            $('#list li').attr('class', 'unchecked'); 
+            $('#check').text("Check All");
+        }
+        $("#opt ul").fadeOut('fast');
+    });
+
+    $("#clear").click(function() {
+        $('#list').html('');
+    });
+
     getData();
+
+    function check(){
+        if($('#list li').length > $('.checked').length)
+            $('#check').text("Check All");
+        else
+            $('#check').text("Unheck All");
+    }
 });
